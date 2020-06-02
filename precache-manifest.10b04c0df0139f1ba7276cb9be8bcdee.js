5ce5611ef7b25616a4f0e2a375b16658c1a91539
Taniyah-L-Jackson <taniyah-l-jackson-h@gmail.com>
2020-06-02 11:50:34 -0500

    Updates

---
+++ precache-manifest.10b04c0df0139f1ba7276cb9be8bcdee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b145f31414499e65f934cef5f25b404",
+    "url": "/cra-sweet-sailings/index.html"
+  },
+  {
+    "revision": "3e9a2194bfe3da2e82fc",
+    "url": "/cra-sweet-sailings/static/js/2.2258e967.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra-sweet-sailings/static/js/2.2258e967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900da00279752e2023d8",
+    "url": "/cra-sweet-sailings/static/js/main.644dbda5.chunk.js"
+  },
+  {
+    "revision": "4afb14b077c236655964",
+    "url": "/cra-sweet-sailings/static/js/runtime-main.b015b892.js"
+  }
+]);
